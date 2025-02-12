c92f927a77a083dd83d59dd6dab16e3365e55187
Skanda Kumar Srikanta <skandonkumar@gmail.com>
2025-02-12 09:52:44 -0500

    Initial Commit

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb+srv://skandonkumar:Gs5mK82DtJXIr7e6@cluster0.6vywd.mongodb.net/advnode?retryWrites=true&w=majority&appName=Cluster0",
+  cookieKey: "123123123",
+};
